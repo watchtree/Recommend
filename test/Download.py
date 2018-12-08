857224d0b5495a19af4b57a942326c59747f2a06
wtree <547552336@qq.com>
2018-12-08 15:19:02 +0800

    SecondWeek 简单推荐系统 MF

---
+++ test/Download.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+# author:wttree
+# datetime:2018/12/5 10:15
+# software: PyCharm
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import numpy as np
+from collections import Counter
+import tensorflow as tf
+
+import os
+import pickle
+import re
+from tensorflow.python.ops import math_ops
+from urllib.request import urlretrieve
+from os.path import isfile, isdir
+from tqdm import tqdm
+import zipfile
+import hashlib
+
+def _unzip(save_path, _, database_name, data_path):
+    """
+    Unzip wrapper with the same interface as _ungzip
+    使用与_ungzip相同的接口解压缩包装器
+    :param save_path: The path of the gzip files
+    :param database_name: Name of database
+    :param data_path: Path to extract to
+    :param _: HACK - Used to have to same interface as _ungzip
+    """
+    print('Extracting {}...'.format(database_name))
+    with zipfile.ZipFile(save_path) as zf:
+        zf.extractall(data_path)
+
+def download_extract(database_name, data_path):
+    """
+    Download and extract database
+    下载并提取数据库
+    :param database_name: Database name
+    """
+    DATASET_ML1M = 'ml-1m'
+
+    if database_name == DATASET_ML1M:
+        url = 'http://files.grouplens.org/datasets/movielens/ml-1m.zip'
+        hash_code = 'c4d9eecfca2ab87c1945afe126590906'
+        extract_path = os.path.join(data_path, 'ml-1m')
+        save_path = os.path.join(data_path, 'ml-1m.zip')
+        extract_fn = _unzip
+
+    if os.path.exists(extract_path):
+        print('Found {} Data'.format(database_name))
+        return
+
+    if not os.path.exists(data_path):
+        os.makedirs(data_path)
+
+    if not os.path.exists(save_path):
+        with DLProgress(unit='B', unit_scale=True, miniters=1, desc='Downloading {}'.format(database_name)) as pbar:
+            urlretrieve(
+                url,
+                save_path,
+                pbar.hook)
+
+    assert hashlib.md5(open(save_path, 'rb').read()).hexdigest() == hash_code, \
+        '{} file is corrupted.  Remove the file and try again.'.format(save_path)
+
+    os.makedirs(extract_path)
+    try:
+        extract_fn(save_path, extract_path, database_name, data_path)
+    except Exception as err:
+        shutil.rmtree(extract_path)  # Remove extraction folder if there is an error
+        raise err
+
+    print('Done.')
+    # Remove compressed data
+#     os.remove(save_path)
+
+class DLProgress(tqdm):
+    """
+    Handle Progress Bar while Downloading
+    下载时处理进度条的模板
+    """
+    last_block = 0
+
+    def hook(self, block_num=1, block_size=1, total_size=None):
+        """
+        A hook function that will be called once on establishment of the network connection and
+        once after each block read thereafter.
+        :param block_num: A count of blocks transferred so far
+        :param block_size: Block size in bytes
+        :param total_size: The total size of the file. This may be -1 on older FTP servers which do not return
+                            a file size in response to a retrieval request.
+        """
+        self.total = total_size
+        self.update((block_num - self.last_block) * block_size)
+        self.last_block = block_num
+
+data_dir = './'
+#当前运行文件目录下
+download_extract('ml-1m', data_dir)
